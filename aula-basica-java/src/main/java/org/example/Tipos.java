1bc663e8ebd25dc651c79a017587568042018784
Lucas Gabriel <lucasgabriel19672@gmail.com>
2025-08-08 18:59:53 -0300

    Add files via upload

---
+++ aula-basica-java/src/main/java/org/example/Tipos.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Tipos {
+    public static void main(String[] args) {
+        String nome = "carlos";
+        int idade = 50;
+        double peso = 80.0;
+
+        final String CPf = "123.456.789-00";
+
+        System.out.println("O nome do usuario é " + nome);
+        System.out.println("O cpf do usuario é " + CPf);
+        System.out.println("A idade do usuario é " + idade);
+        System.out.println("O peso do usuario é " + peso );
+    }
+}
