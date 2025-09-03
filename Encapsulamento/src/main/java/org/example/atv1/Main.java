79d23823169fded2b7d968b262126eacb6cf58a6
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-03 20:34:18 -0300

    feat polimorfismo

--- Encapsulamento/src/main/java/org/example/atv1/Main.java
+++ Encapsulamento/src/main/java/org/example/atv1/Main.java
@@ -7,14 +7,11 @@ public class Main {
         Pato pato = new Pato();
         Galo galo = new Galo();
 
-        System.out.println(cachorro.emitirSom());
-        System.out.println(cachorro.comer());
-        System.out.println(gato.emitirSom());
-        System.out.println(gato.comer());
-        System.out.println(pato.emitirSom());
-        System.out.println(pato.comer());
-        System.out.println(galo.emitirSom());
-        System.out.println(galo.comer());
+        System.out.println("O cachorro faz " + cachorro.emitirSom() + " ele come:" + cachorro.comer());
+        System.out.println("O gato faz: " + gato.emitirSom() + " ele come: " + gato.comer());
+        System.out.println("O pato faz: " + pato.emitirSom() + " ele come: " + pato.comer());
+        System.out.println("O galo faz: " + galo.emitirSom() + " ele come: " + galo.comer());
+
 
 
     }
