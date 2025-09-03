dbb68a8f351a4d0f5bc1f591836f11b568639d6f
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-03 20:56:40 -0300

    feat;

--- Encapsulamento/src/main/java/org/example/atv2/Main.java
+++ Encapsulamento/src/main/java/org/example/atv2/Main.java
@@ -2,7 +2,14 @@ package org.example.atv2;
 
 public class Main {
     public static void main(String[] args) {
+        Soma soma = new Soma();
+        Subtracao subtracao = new Subtracao();
+        Multiplicacao multiplicacao = new Multiplicacao();
+        Divisao divisao = new Divisao();
 
-
+        System.out.println(soma.calcular(2, 4));
+        System.out.println(subtracao.calcular(2, 4));
+        System.out.println(multiplicacao.calcular(2, 4));
+        System.out.println(divisao.calcular(2,4));
     }
 }
